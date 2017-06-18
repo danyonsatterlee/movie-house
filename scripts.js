5c8348b4c4d2ae50ef30b4fa021d994ac4febb17
danyonsatterlee <danyon.satterlee@gmail.com>
2017-06-18 12:44:30 -0600

    now showing mostly layed out correctly

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+$('.navbar-toggle').on('click', function() {
+ $('.after-nav').css('margin-top','200px');
+});
 
+$(".navbar-toggle").on("click", function () {
+    $(this).toggleClass("active");
+});
